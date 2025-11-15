815c77f77fbb13d89dfac72d59c09c4e2284f789
BerkErdogann <berk.erd00@gmail.com>
2025-11-15 12:39:35 +0300

    Her yerde renkler kimin bu renkler bilemiyorum

--- prove_me_wrong/lib/main.dart
+++ prove_me_wrong/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+final colorScheme = const ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xffa31e1e),
+  onPrimary: Color(0xffede3d7),
+  onPrimaryContainer: Color(0xff891b1b),
+  secondary: Color(0xff6b2f2f),
+  onSecondary: Color(0xfff7eaea),
+  tertiary: Color(0xffd87070),
+
+  //  Hata vermesin diye ekledim color scheme için zorunlu
+  error: Colors.red,
+  onError: Colors.red,
+  //
+  surface: Color(0xffa31e1e),
+  onSurface: Color(0xfff7eaea),
+  surfaceContainer: Color(0xff891b1b),
+);
 void main() {
   runApp(MaterialApp(home: Empty()));
 }
