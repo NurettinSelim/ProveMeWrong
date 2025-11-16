9aafa338fa11d7bf39d135c5e4e5e9805ae59b71
Berra Eğcin <berraegcin@gmail.com>
2025-11-16 19:27:18 +0300

    BİZİM RENK FONT MESAİSİ DEVAM EDİYOO
    -Fontlar pubspec'e eklendi -main'de color sheme yapmak yeine AppColor oluşturuldu böylece error tarzı şeyler yapmaya gerek kalmadı. İlerdeki versiyonlarda light theme'e direkt AppColor konulabilirmiş, sorun yok gibi geldi o yüzden - 11.16.25

---
+++ prove_me_wrong/lib/core/theme/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._(); // private constructor
+
+  static const primary = Color(0xffa31e1e);          // ana kırmızı
+  static const onPrimary = Color(0xffede3d7);        // background, card içi yazı
+  static const primaryContainer = Color(0xff891b1b); // card içi yazı arka plan, koyu kırmızı
+
+  static const secondary = Color(0xff6b2f2f);        // random ve signup sayfası -- en koyu kırmızı
+  static const onSecondary = Color(0xfff7eaea);      // kategori içi yazı -- açık pembe
+
+  static const tertiary = Color(0xffd87070);         // room oluşturma, menü bar highligtlama -- pembe
+}
