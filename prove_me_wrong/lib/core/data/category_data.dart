3546e1de1a155c1c6b4cd38cd4ee82646d38e39a
BerkErdogann <berk.erd00@gmail.com>
2025-11-16 23:17:38 +0300

    Categories enum

---
+++ prove_me_wrong/lib/core/data/category_data.dart
@@ -0,0 +1,13 @@
+enum Categories {
+  economy("Economy"),
+  politics("Politics"),
+  games("Games"),
+  musics("Musics"),
+  history("History"),
+  books("Books"),
+  others("Others"),
+  foods("Foods");
+
+  final String value;
+  const Categories(this.value);
+}
