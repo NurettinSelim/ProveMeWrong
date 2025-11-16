4413687c67593703aeeba2175072c75546d65c58
BerkErdogann <berk.erd00@gmail.com>
2025-11-16 22:22:30 +0300

    Empty() -> App()

--- prove_me_wrong/lib/main.dart
+++ prove_me_wrong/lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:prove_me_wrong/Widgets/footer.dart';
+import 'package:prove_me_wrong/core/theme/app_theme.dart';
 
 void main() {
-  runApp(MaterialApp(home: Empty()));
+  runApp(MaterialApp(home: App()));
 }
 
-class Empty extends StatelessWidget {
-  const Empty({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.onPrimary,
+      bottomNavigationBar: Footer(),
+    );
   }
 }
