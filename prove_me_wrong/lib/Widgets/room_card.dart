d2a811c9a610ca536f72ab002b521c192464ae53
BerkErdogann <berk.erd00@gmail.com>
2025-11-17 16:41:29 +0300

    RoomCard

---
+++ prove_me_wrong/lib/Widgets/room_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:prove_me_wrong/core/data/room_data.dart';
+import 'package:prove_me_wrong/core/theme/app_theme.dart';
+
+class RoomCard extends StatelessWidget {
+  const RoomCard({super.key, required this.room});
+
+  final Room room;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: AppColors.primary,
+        borderRadius: BorderRadius.circular(6),
+        border: BoxBorder.all(width: 1.5),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 6),
+            decoration: BoxDecoration(
+              color: AppColors.primaryContainer,
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Text(
+              room.title,
+              style: TextStyle(
+                color: AppColors.onSecondary,
+                fontSize: 18,
+                fontFamily: "Azer29LT",
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "(${room.ownerScore}) | ${room.language.value} | ${room.category.value}",
+                style: TextStyle(
+                  color: AppColors.onSecondary,
+                  fontFamily: "Azer29LT",
+                  fontSize: 14,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Enter",
+                  style: TextStyle(
+                    color: AppColors.onSecondary,
+                    fontFamily: "Azer29LT",
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
