336827fb0aa60fc738d76cf3679ff34f0a258259
BerkErdogann <berk.erd00@gmail.com>
2025-12-08 00:50:39 +0300

    fromString fonksiyonlarını ekledim

--- prove_me_wrong/lib/core/data/category_data.dart
+++ prove_me_wrong/lib/core/data/category_data.dart
@@ -10,6 +10,15 @@ enum Categories {
 
   final String value;
   const Categories(this.value);
+
+  static Categories? fromString(String category) {
+    for (final cat in Categories.values) {
+      if (cat.value == category) {
+        return cat;
+      }
+    }
+    return null;
+  }
 }
 
 bool isValidCategory(String category) {
--- prove_me_wrong/lib/core/data/language_data.dart
+++ prove_me_wrong/lib/core/data/language_data.dart
@@ -6,6 +6,15 @@ enum Languages {
 
   final String value;
   const Languages(this.value);
+
+  static Languages? fromString(String value) {
+    for (final lang in Languages.values) {
+      if (lang.value == value) {
+        return lang;
+      }
+    }
+    return null;
+  }
 }
 
 bool isValidLanguage(String language) {
