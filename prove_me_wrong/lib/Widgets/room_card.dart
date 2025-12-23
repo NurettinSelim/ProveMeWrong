1736acd7b41c32556ff92f7307945e8190f7ed22
BerkErdogann <berk.erd00@gmail.com>
2025-12-23 13:55:17 +0300

    PopUp == true ReportPopUp göster

--- prove_me_wrong/lib/Widgets/room_card.dart
+++ prove_me_wrong/lib/Widgets/room_card.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:prove_me_wrong/Widgets/report.dart';
 import 'package:prove_me_wrong/core/data/room_data.dart';
 import 'package:prove_me_wrong/core/theme/app_theme.dart';
 
 class RoomCard extends StatelessWidget {
-  const RoomCard({super.key, required this.room, this.onEnter});
+  const RoomCard({
+    super.key,
+    required this.room,
+    this.onEnter,
+    this.showPopUp = true,
+  });
 
+  final bool showPopUp;
   final Room room;
   final void Function(String roomId)? onEnter;
 
@@ -20,6 +27,19 @@ class RoomCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          showPopUp
+              ? Align(
+                  alignment: AlignmentGeometry.centerRight,
+                  child: ReportPopUp(
+                    reasons: RoomReportReasons.values,
+                    reportedRoomID: room.roomId,
+                    reportedUserID: room.ownerId,
+                    category: room.category,
+                    title: room.title,
+                  ),
+                )
+              : const SizedBox.shrink(),
+          SizedBox(height: 8),
           Container(
             padding: EdgeInsets.symmetric(vertical: 12, horizontal: 6),
             decoration: BoxDecoration(
