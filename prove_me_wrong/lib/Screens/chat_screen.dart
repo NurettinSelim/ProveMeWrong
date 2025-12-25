c519d022891a25cecb60bcbf0637b271d89e8741
Berra Eğcin <berraegcin@gmail.com>
2025-12-25 20:33:12 +0300

    AAAA PATRON PUSHLAMADIĞIM GÜNLERİN WHOOPERINI KESMEEEE :(((((((

--- prove_me_wrong/lib/Screens/chat_screen.dart
+++ prove_me_wrong/lib/Screens/chat_screen.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:prove_me_wrong/core/theme/app_theme.dart';
 import 'package:prove_me_wrong/core/data/room_data.dart';
 import 'package:prove_me_wrong/widgets/chat_bubble.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key, required this.room});
+//rulesda $message .write kısmında: null && root.child('users/' + auth.uid + '/rooms/' + $id).exists() && newData.child('senderId').val() === auth.uid
 
-  final Room room;
+class ChatScreen extends StatefulWidget {
+  final Room rooms;
+
+  const ChatScreen({
+    super.key,
+    required this.rooms,
+  }); // required room = chatscreen başka screende çağırıldığında yazılacak
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final currentUser = FirebaseAuth.instance.currentUser;
+
+  late final userDb = FirebaseDatabase.instance.ref(
+    "users/${currentUser!.uid}",
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final TextEditingController messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
+  }
+
+  Future<void> sendMessage(String text) async {
+    final userID = FirebaseAuth
+        .instance
+        .currentUser!
+        .uid; //ownerID yaparsam sadece room sahibi mesaj atabilir, o yüzden direkt uygulamayı açan kişinin idsini alıyorum
+    final roomId = widget.rooms.roomId;
+
+    final messageRef = FirebaseDatabase.instance
+        .ref("rooms/$roomId/messages")
+        .push();
+
+    await messageRef.set({
+      "message": text,
+      "timeStamp": ServerValue.timestamp,
+      "senderId": userID,
+    });
+    messageController.clear();
+  }
+
+  Stream<DatabaseEvent> messageStream() {
+    final roomid = widget.rooms.roomId;
+    return FirebaseDatabase.instance
+        .ref("rooms/$roomid/messages")
+        .orderByChild("timeStamp")
+        .onValue; //değişiklik olması durumunda
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +78,7 @@ class ChatScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  room.title,
+                  widget.rooms.title,
                   style: TextStyle(
                     color: AppColors.onPrimary,
                     fontFamily: "SpaceMono",
@@ -29,17 +87,22 @@ class ChatScreen extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  "${room.ownerScore} | ${room.language.name} | ${room.category.name}",
+                  "${widget.rooms.ownerScore} | ${widget.rooms.language.name} | ${widget.rooms.category.name}",
                   style: TextStyle(
                     fontFamily: "Azer29LT",
-                    fontSize: 12,
+                    fontSize: 16,
                     color: AppColors.onPrimary,
                   ),
                 ),
               ],
             ),
             Spacer(),
-            Icon(Icons.arrow_back, color: AppColors.onSecondary),
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back, color: AppColors.onSecondary),
+            ),
           ],
         ),
         backgroundColor: AppColors.primaryContainer,
@@ -48,255 +111,53 @@ class ChatScreen extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: ListView(
-                padding: EdgeInsets.all(12),
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "Hello! Welcome to the chat.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "Thank you! Glad to be here.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "I'm thinking you are wrong.",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ChatBubble(
-                      text: "What do you think?",
-                      isSentByMe: true,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ChatBubble(
-                      text: "No, I believe I'm right.",
-                      isSentByMe: false,
-                    ),
-                  ),
-                ],
+              child: StreamBuilder(
+                stream: messageStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData ||
+                      snapshot.data!.snapshot.value == null) {
+                    return Center(
+                      child: Text(
+                        "Start the Conversation",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: "SpaceMono",
+                          fontSize: 20,
+                        ),
+                      ),
+                    );
+                  }
+
+                  final data = snapshot.data!.snapshot.value as Map;
+                  final messages = data.values.toList();
+
+                  messages.sort(
+                    (a, b) => a["timeStamp"].compareTo(b["timeStamp"]),
+                  );
+
+                  return ListView.builder(
+                    padding: EdgeInsets.all(12),
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final msg = messages[index];
+                      final isMe =
+                          msg["senderId"] ==
+                          FirebaseAuth.instance.currentUser!.uid;
+                      return Align(
+                        alignment: isMe
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: ChatBubble(
+                          text: msg["message"],
+                          isSentByMe: isMe,
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
             ),
           ],
@@ -309,6 +170,7 @@ class ChatScreen extends StatelessWidget {
           children: [
             Expanded(
               child: TextField(
+                controller: messageController,
                 decoration: InputDecoration(
                   hintText: "Start the Fight...",
                   hintStyle: TextStyle(
@@ -335,7 +197,10 @@ class ChatScreen extends StatelessWidget {
             SizedBox(width: 8),
             IconButton(
               icon: Icon(Icons.send, color: AppColors.onPrimary),
-              onPressed: () {},
+              onPressed: () {
+                final text = messageController.text;
+                sendMessage(text);
+              },
             ),
           ],
         ),
