0fc2f15ddbf358ed93b198f13d2aeabe59da83c9
BerkErdogann <berk.erd00@gmail.com>
2026-03-17 17:48:45 +0300

    Burada alignment a gerek yok alignment chat_screen de uygulanıyor

--- prove_me_wrong/lib/widgets/chat_bubble.dart
+++ prove_me_wrong/lib/widgets/chat_bubble.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart ';
-import 'package:prove_me_wrong/widgets/bubble_paint.dart';
 import 'package:prove_me_wrong/core/theme/app_theme.dart';
+import 'package:prove_me_wrong/widgets/bubble_paint.dart';
 
 class ChatBubble extends StatelessWidget {
   final String text;
@@ -9,31 +9,28 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: isSentByMe ? Alignment.centerRight : Alignment.centerLeft,
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 4),
-        child: Column(
-          children: [
-            CustomPaint(
-              painter: ChatBubblePainter(
-                color: isSentByMe ? AppColors.tertiary : AppColors.primary,
-                isSender: isSentByMe,
-              ),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(12, 8, 12, 8),
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    color: AppColors.onPrimary,
-                    fontSize: 16,
-                    fontFamily: "Azer29LT",
-                  ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 4),
+      child: Column(
+        children: [
+          CustomPaint(
+            painter: ChatBubblePainter(
+              color: isSentByMe ? AppColors.tertiary : AppColors.primary,
+              isSender: isSentByMe,
+            ),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(12, 8, 12, 8),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: AppColors.onPrimary,
+                  fontSize: 16,
+                  fontFamily: "Azer29LT",
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
